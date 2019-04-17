9ad91e14548538aa5bcb629cd1306060ee55b152
Fabrizio <fabrizioojeda@hotmail.it>
2019-04-17 13:47:10 +0200

    17-04-19
    Aggiunta variabili

--- CacciaPiGreco/CacciaPiGreco.ino
+++ CacciaPiGreco/CacciaPiGreco.ino
@@ -1,6 +1,26 @@
+int btn_Inizio;
+int btn_Uno;
+int btn_Due;
+int btn_Tre;
+int btn_Quattro;
+int btn_Cinque;
+
 void setup() {
   // put your setup code here, to run once:
 
+btn_Inizio  = 7;
+btn_Uno     = 8;
+btn_Due     = 9;
+btn_Tre     = 10;
+btn_Quattro = 11;
+btn_Cinque  = 12;
+
+pinMode(btn_Inizio,  INPUT);
+pinMode(btn_Uno,     INPUT);
+pinMode(btn_Due,     INPUT);
+pinMode(btn_Tre,     INPUT);
+pinMode(btn_Quattro, INPUT);
+pinMode(btn_Cinque,  INPUT);
 }
 
 void loop() {
